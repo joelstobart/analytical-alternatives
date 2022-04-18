3c53220f7b777c9374afc3ef81d5aa0b05f47439
Joel Stobart <joel.stobart@landregistry.gov.uk>
2022-04-18 21:45:22 +0100

    Early version

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,30 +1,95 @@
 import * as React from "react"
 import { Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
+import island from "../images/island.png"
+import background from "../images/commons-fog.png";
 
 const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["auto", "webp", "avif"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
-    <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link> <br />
-      <Link to="/using-ssr">Go to "Using SSR"</Link> <br />
-      <Link to="/using-dsg">Go to "Using DSG"</Link>
-    </p>
+      <main>
+      <section>
+      <div id={"london"} style={{
+          backgroundImage: `url(${background})`,
+          backgroundSize: `cover`,
+          backgroundPosition: `center`,
+          height: `80vh`,}}>
+          <div
+              style={{
+                  margin: `0 auto`,
+                  maxWidth: 960,
+                  padding: `1.45rem 1.0875rem`,
+                  paddingTop: `37vh`,
+                  textAlign: `center`
+              }}
+          >
+              <h2 style={{ margin: `0 0 2rem 0`, fontSize: `60px`, position:"sticky", top:-1  }}>
+                  <Link
+                      to="/"
+                      style={{
+                          fontFamily: `Courier New` ,
+                          fontWeight: `lighter`,
+                          color: `white`,
+                          textShadow: `0 0 22px #64ce5c, 0 0 30px #64ce5c, 0 0 40px #64ce5c`,
+                          textDecoration: `none`
+                      }}
+                  >
+                      no nonsense.
+                  </Link>
+              </h2>
+          </div>
+      </div>
+
+          </section>
+          <section>
+      <div style={{
+          backgroundColor: `black`,
+          backgroundSize: `cover`,
+          backgroundPosition: `center`,
+          height: `80vh`}}>
+
+          <div
+              style={{
+                  margin: `0 auto`,
+                  maxWidth: 960,
+                  padding: `1.45rem 1.0875rem`,
+                  paddingTop: `20vh`,
+                  textAlign: `center`
+              }}
+          >
+              <h2 style={{ margin: `0 0 2rem 0`, fontSize: `60px`, position:"sticky", top:0  }}>
+                  <Link
+                      to="/"
+                      style={{
+                          fontFamily: `Courier New` ,
+                          fontWeight: `lighter`,
+                          color: `white`,
+                          textShadow: `0 0 22px #64ce5c, 0 0 30px #64ce5c, 0 0 40px #64ce5c`,
+                          textDecoration: `none`
+                      }}
+                  >
+                      no nonsense.
+                  </Link>
+              </h2>
+              <p style={{ color:`white`, fontFamily:`calibri, Sans-serif`, fontSize: `26px`, textAlign:`left`, lineHeight:`1.4em`}}>
+                  A trusted, global risk management consultancy that has considerable experience in providing market-leading specialist intelligence and investigations services to its clients.  Each client is provided a solution based on our unique convergence of diverse disciplines from both public and private sectors. We believe in a ‘no-nonsense’, honest and open approach to our work and as such are widely respected.
+              </p>
+          </div>
+      </div>
+
+          </section>
+          <section>
+      <div style={{
+          backgroundImage: `url(${island})`,
+          backgroundSize: `cover`,
+          backgroundPosition: `center`,
+          height: `80vh`,}}>
+
+      </div>
+      </section>
+      </main>
   </Layout>
 )
 
