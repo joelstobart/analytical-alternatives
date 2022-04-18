c2379f88c7b2e6e49d92363fadf1d3edc8375da4
Joel Stobart <joel.stobart@landregistry.gov.uk>
2022-04-18 21:33:30 +0100

    Push various changes

--- src/components/header.js
+++ src/components/header.js
@@ -1,33 +1,28 @@
 import * as React from "react"
 import PropTypes from "prop-types"
-import { Link } from "gatsby"
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
+import aal from "../images/analytical-white.svg"
+
+const Header = () => (
+  <header style={{
+          height: `15vh`,
+      top:-1,
+      position:`sticky` }}>
+      <div style={{
+          height: `15vh`,
+          marginTop:`-15vh`,
+          backgroundColor: `rgba(0,0,0,0.1)`,
+          color: `#a7a7a7`}}>
+          <div style={{
+                  margin:`0 auto`,
+                  maxWidth: 960,
+                  paddingTop: `3vh`,
+                  paddingBottom: `3vh`,
+                  textAlign: `center`
+          }}>
+              <img src={aal} height={`80vh`} align={`center`} alt={`Analytical Alternatives Logo`}/>
+          </div>
+      </div>
   </header>
 )
 
