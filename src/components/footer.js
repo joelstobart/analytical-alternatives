6513c897029a0de8da85653cdd02c64927b79de7
Joel Stobart <joel.stobart@landregistry.gov.uk>
2022-12-02 10:46:55 +0000

    Add the ISO logo

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,6 +2,8 @@ import * as React from "react"
 import { Link } from "gatsby"
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+import iso from "../images/iso9001.svg"
 import { faHouseTurret, faPhoneRotary, faFileShield, faCookieBite, faHandshakeAngle } from '@fortawesome/pro-duotone-svg-icons'
 const Footer = () => (
   <footer>
@@ -14,6 +16,7 @@ const Footer = () => (
               Hampshire
               GU14 7LY
           </address>
+
           <div>
               <span className={"telephone"}><FontAwesomeIcon icon={faPhoneRotary} />&nbsp;<a href="tel:+44128530110">+44 (0) 1628 530110</a></span>
               &nbsp; &nbsp; &nbsp;
@@ -25,24 +28,19 @@ const Footer = () => (
                   Cookies: We work hard to protect your privacy and cookie banners are tiresome.
                   We don't use non essential cookies.
               </span>
-
           </div>
-          <div style={{marginTop: `5vh`}}>
 
+          <div style={{marginTop: `5vh`}}>
+                <div style={{float:"left", height:"200px",marginRight:"20px"}}>
+                    <img src={`${iso}`} alt={"ISO 9001"}/>
+                </div>
               <span>
-              <FontAwesomeIcon icon={faHandshakeAngle} /> We commit to the highest <Link to={"standards"}>investigative, ethical and legal standards</Link>
+                <FontAwesomeIcon icon={faHandshakeAngle} /> We commit to the highest <Link to={"standards"}>investigative, ethical and legal standards</Link>
                   , <Link to={"standards"}>professional standards including ISO 9001:2015 certified</Link>
                   , <Link to={"standards"}>information security standards</Link>, safeguarding your data and ensuring your privacy.
               </span>
           </div>
-
-
-
-
-
-          <div>
-
-          </div>
+          <div></div>
       </div>
   </footer>
 )
